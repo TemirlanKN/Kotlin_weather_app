0a1c55ad04a30831bbfd253636c4fc36900dedcd
TemirlanKN <temirlan9908@gmail.com>
2022-07-26 22:49:20 +1300

    Some commit to save changes because i want to eat

---
+++ app/src/main/java/com/trasty/kotlin_weather_app/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.trasty.kotlin_weather_app
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    val liveDataCurrent = MutableLiveData<String>()
+    val liveDataList = MutableLiveData<String>()
+}
