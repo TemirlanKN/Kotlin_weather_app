ab61618f85177233a39004c42b66bdcc438d81c1
TemirlanKN <temirlan9908@gmail.com>
2022-08-17 03:51:36 +1300

    TabLayoutMediator and Fragment attachments

--- app/src/main/java/com/trasty/kotlin_weather_app/fragments/MainFragment.kt
+++ app/src/main/java/com/trasty/kotlin_weather_app/fragments/MainFragment.kt
@@ -8,11 +8,20 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.FragmentActivity
+import com.google.android.material.tabs.TabLayoutMediator
 import com.trasty.kotlin_weather_app.R
+import com.trasty.kotlin_weather_app.adapters.VpAdapter
 import com.trasty.kotlin_weather_app.databinding.FragmentMainBinding
 import java.util.jar.Manifest
 
 class MainFragment : Fragment() {
+    private val fList = listOf(
+        HoursFragment.newInstance(), DaysFragment.newInstance()
+    )
+    private val tList = listOf(
+        "Hours", "Days"
+    )
     private lateinit var pLauncher: ActivityResultLauncher<String>
     private lateinit var binding: FragmentMainBinding
 
@@ -27,6 +36,15 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         checkPermission()
+        init()
+    }
+
+    private fun init() = with(binding){
+        val adapter = VpAdapter(activity as FragmentActivity, fList)
+        vp.adapter = adapter
+        TabLayoutMediator(tabLayout, vp){
+            tab, pos -> tab.text = tList[pos]
+        }.attach()
     }
 
     private fun permissionListener(){
