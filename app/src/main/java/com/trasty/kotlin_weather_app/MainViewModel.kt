739928503be897e3035075db19b054f54bbdf37b
TemirlanKN <temirlan9908@gmail.com>
2022-08-30 00:57:42 -0400

    CardView, Data class for list items

--- app/src/main/java/com/trasty/kotlin_weather_app/MainViewModel.kt
+++ app/src/main/java/com/trasty/kotlin_weather_app/MainViewModel.kt
@@ -2,8 +2,9 @@ package com.trasty.kotlin_weather_app
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.trasty.kotlin_weather_app.adapters.WeatherModel
 
 class MainViewModel : ViewModel() {
-    val liveDataCurrent = MutableLiveData<String>()
-    val liveDataList = MutableLiveData<List<String>>()
+    val liveDataCurrent = MutableLiveData<WeatherModel>()
+    val liveDataList = MutableLiveData<List<WeatherModel>>()
 }
